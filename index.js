fef262b6362bee7e4469c0bdf474fb4037aa10eb
juliano <julianopenna@gmail.com>
2018-07-03 20:25:42 +0800

    Change process -> global var

--- index.js
+++ index.js
@@ -12,43 +12,44 @@ const codeMap = new Map([
 ]);
 
 const skipCleanup = Symbol('skipCleanup');
-let errorsLog = () => {};
-let debugError = () => {};
+let logger = () => {};
+let fileLogger = () => {};
 
 function setup({
   callbacks = () => {},
   logPath,
   debugLabel,
-  logger,
-  fileLogger,
+  logger: loggerParam,
+  fileLogger: fileLoggerParam,
   customCodeMap,
   timeoutAfter = 5000,
 }) {
   const callbacksArray = Array.isArray(callbacks) ? callbacks : [callbacks];
   if (customCodeMap) customCodeMap.forEach((a, b) => codeMap.set(b, a));
 
-  if (logPath) errorsLog = nodeLogger.createSimpleFileLogger(logPath);
-  if (debugLabel) debugError = debug(debugLabel);
+  if (logPath) logger = nodeLogger.createSimpleFileLogger(logPath);
+  if (debugLabel) fileLogger = debug(debugLabel);
 
-  if (logger) errorsLog = fileLogger;
-  if (fileLogger) debugError = logger;
+  if (loggerParam) logger = loggerParam;
+  if (fileLoggerParam) fileLogger = fileLoggerParam;
 
 
   const extraInfoSymbol = Symbol('extraInfoSymbol');
+  global[extraInfoSymbol] = {};
 
   // Capture Errors not caught and start cleanup
   process.on('uncaughtException', (err) => {
     const errorMsg = err;
-    (errorsLog.error || errorsLog)(`Uncaught Exception -> ${errorMsg.stack}`);
-    debugError(`Uncaught Exception -> ${errorMsg.stack}`);
+    (logger.error || logger)(`Uncaught Exception -> ${errorMsg.stack}`);
+    fileLogger(`Uncaught Exception -> ${errorMsg.stack}`);
     process.emit('cleanup', 1);
   });
 
   // Capture Promise rejections not handled and start cleanup
   process.on('unhandledRejection', (reason, p) => {
     const errorMsg = `Promise: ${reason}`;
-    (errorsLog.error || errorsLog)(`Unhandled Rejection -> ${errorMsg}\n`, p);
-    debugError(`Unhandled Rejection -> ${errorMsg}\n`, p);
+    (logger.error || logger)(`Unhandled Rejection -> ${errorMsg}\n`, p);
+    fileLogger(`Unhandled Rejection -> ${errorMsg}\n`, p);
     process.emit('cleanup', 2);
   });
 
@@ -60,51 +61,53 @@ function setup({
   process.on('SIGUSR2', () => process.emit('cleanup', 5));
 
   process.on('exit', (code) => {
-    if (process[extraInfoSymbol]) {
-      const extraInfo = JSON.stringify(process[extraInfoSymbol], null, 2);
-      debugError(`Graceful Exit extra info: ${extraInfo}`);
-      (errorsLog.info || errorsLog)(extraInfo);
+    if (Object.keys(global[extraInfoSymbol]).length) {
+      const extraInfo = JSON.stringify(global[extraInfoSymbol], null, 2);
+      fileLogger(`Graceful Exit extra info: ${extraInfo}`);
+      (logger.info || logger)(extraInfo);
     }
 
     const errorMsg = `(PID ${process.pid}) Exiting with code: ${code} - ${codeMap.get(code)}`;
-    (errorsLog.info || errorsLog)(errorMsg);
-    debugError(errorMsg);
+    (logger.info || logger)(errorMsg);
+    fileLogger(errorMsg);
   });
 
   process.on('quit', code => process.emit('cleanup', code || 101));
 
   process.on('cleanup', (code) => {
-    if (process[skipCleanup]) return debugError('CLEANUP WAS CALLED AGAIN!!! There is some error leaking in the cleanup process.');
-    process[skipCleanup] = true;
+    if (global[skipCleanup]) return fileLogger('CLEANUP WAS CALLED AGAIN!!! There is some error leaking in the cleanup process.');
+    global[skipCleanup] = true;
     callbacksArray.forEach(cb => cb(code, codeMap.get(code)));
 
     // Set cleanup timeout
     setTimeout(() => {
       process.exit(code);
-      debugError('Cleanup Timeout');
-      (errorsLog.info || errorsLog)('Cleanup Timeout');
+      fileLogger('Cleanup Timeout');
+      (logger.info || logger)('Cleanup Timeout');
     }, timeoutAfter);
   });
 
-  return ({ symbols: { extraInfoSymbol } });
+  return { extraInfoSymbol };
 }
 
 let cleanupsCount = 0;
 let cleanupsRan = 0;
+let cleanupsFinished = 0;
 
 function gracefulExit(callback = () => { }) {
   cleanupsCount++;
 
   process.on('cleanup', async (code) => {
-    if (process[skipCleanup]) return;
-    debugError('CLEANUP GRACEFULLY', cleanupsCount, cleanupsRan);
+    if (cleanupsRan >= cleanupsCount) return;
+    cleanupsRan++;
+    fileLogger('CLEANUP GRACEFULLY', cleanupsCount, cleanupsFinished);
     try {
       await callback();
     } catch (e) {
-      debugError('Error on cleanup callback', e);
+      fileLogger('Error on cleanup callback', e);
     }
-    cleanupsRan++;
-    if (cleanupsCount === cleanupsRan) process.exit(code);
+    cleanupsFinished++;
+    if (cleanupsCount === cleanupsFinished) process.exit(code);
   });
 }
 
